8ab57252463aff3eedc5648291e9497ce399b051
PabloAja1000 <pa22pu@gmail.com>
2022-08-13 00:53:44 +0200

    Metodos de Recapitulacion y organizacion de Index mayoriatariamente completdos 12/08/2022

---
+++ Codigo Extra.vb
@@ -0,0 +1,7 @@
+﻿Public Class Codigo_Extra
+    'Dim proyectFolder As String = IO.Path.GetFileName(proyectPath)
+    'Public indexFile As String = My.Computer.FileSystem.GetFiles(proyectPath, FileIO.SearchOption.SearchAllSubDirectories, proyectFolder & ".index").First
+
+    'InStrRev(proyectPath, "\")
+    'proyectPath.Substring(16))
+End Class
--- WinRechapter.vb
+++ WinRechapter.vb
@@ -6,9 +6,8 @@
         Dim numChapter = My.Computer.FileSystem.GetFiles(Form1.proyectPath, FileIO.SearchOption.SearchAllSubDirectories, "*.wpok").Count
         nud_rechapters.Maximum = numChapter
         nud_rechapters.Minimum = 1
+        nud_rechapters.Enabled = False
 
-        btn_rechapter.Enabled = False
-        btn_renameChapter.Enabled = False
         Form1.Enabled = False
 
     End Sub
@@ -20,6 +19,7 @@
             lstBox_chapters.Items.Insert(indexChapter - 1, index)
             lstBox_chapters.SetSelected(indexChapter - 1, True)
         End If
+        reindexing()
     End Sub
     Private Sub btn_downChapter_Click(sender As Object, e As EventArgs) Handles btn_downChapter.Click
         Dim indexChapter As Integer = lstBox_chapters.SelectedIndex
@@ -29,35 +29,60 @@
             lstBox_chapters.Items.Insert(indexChapter + 1, index)
             lstBox_chapters.SetSelected(indexChapter + 1, True)
         End If
+        reindexing()
     End Sub
     Private Sub btn_renameChapter_Click(sender As Object, e As EventArgs) Handles btn_renameChapter.Click
-        Dim indexChapter As Integer = lstBox_chapters.SelectedIndex
-        My.Computer.FileSystem.RenameFile(Form1.proyectPath & "\" & lstBox_chapters.SelectedItem & ".wpok", Form1.proyectPath & "\" & txt_rename.Text & ".wpok")
-        lstBox_chapters.Items.RemoveAt(indexChapter)
-        lstBox_chapters.Items.Insert(txt_rename.Text, indexChapter)
+        Try
+            Dim indexChapter As Integer = lstBox_chapters.SelectedIndex
+            Dim newName = Trim(txt_rename.Text)
+            My.Computer.FileSystem.RenameFile(Form1.proyectPath & "\" & lstBox_chapters.SelectedItem & ".wpok", newName)
+            lstBox_chapters.Items.RemoveAt(indexChapter)
+            lstBox_chapters.Items.Insert(indexChapter, txt_rename.Text)
+            reindexing()
+            lstBox_chapters.ClearSelected()
+            txt_rename.Text = ""
+            nud_rechapters.Enabled = False
+        Catch ex As Exception
+            MsgBox("Deebes seleccionar el capitulo que quieres renombrar y no dejar el campo vacio")
+        End Try
 
-        'reloadListChapters()
     End Sub
     Private Sub btn_Rechapter_Click(sender As Object, e As EventArgs) Handles btn_rechapter.Click
-        Dim indexChapter As Integer = lstBox_chapters.SelectedIndex = nud_rechapters.Value - 1
-        ' lstBox_chapters.SetSelected(indexChapter, False)
-        lstBox_chapters.ClearSelected()
+        Try
+            Dim indexChapter As Integer = lstBox_chapters.SelectedIndex
+            Dim index As String = lstBox_chapters.SelectedItem.ToString()
+            Dim newIndex As Integer = nud_rechapters.Value - 1
+            lstBox_chapters.Items.RemoveAt(indexChapter)
+            lstBox_chapters.Items.Insert(newIndex, index)
+            reindexing()
+            lstBox_chapters.ClearSelected()
+            txt_rename.Text = ""
+            nud_rechapters.Enabled = False
+        Catch ex As Exception
+            MsgBox("Deebes seleccionar el capitulo que quieres recapitular")
+        End Try
 
     End Sub
     Private Sub btn_deleteChapter_Click(sender As Object, e As EventArgs) Handles btn_deleteChapter.Click
-        Dim chapter = Form1.proyectPath & "\" & lstBox_chapters.SelectedItem & ".wpok"
-        My.Computer.FileSystem.DeleteFile(chapter)
-        lstBox_chapters.Items.RemoveAt(lstBox_chapters.SelectedIndex)
-        reindexing()
-        'reloadListChapters()
-        lstBox_chapters.ClearSelected()
-        txt_rename.Text = ""
+        Try
+            Dim chapter = Form1.proyectPath & "\" & lstBox_chapters.SelectedItem & ".wpok"
+            My.Computer.FileSystem.DeleteFile(chapter)
+            lstBox_chapters.Items.RemoveAt(lstBox_chapters.SelectedIndex)
+            reindexing()
+            lstBox_chapters.ClearSelected()
+            txt_rename.Text = ""
+            nud_rechapters.Enabled = False
+        Catch ex As Exception
+            MsgBox("Deebes seleccionar el capitulo que quieres borrar")
+        End Try
+
     End Sub
     Private Sub lstBox_chapters_SelectedValueChanged(sender As Object, e As EventArgs) Handles lstBox_chapters.SelectedValueChanged
         Try
             Dim chapter As String = lstBox_chapters.SelectedItem.ToString
             txt_rename.Text = chapter
             nud_rechapters.Value = lstBox_chapters.SelectedIndex + 1
+            nud_rechapters.Enabled = True
         Catch ex As Exception
 
         End Try
@@ -66,25 +91,21 @@
         lstBox_chapters.Items.Clear()
         Dim fileIndex = My.Computer.FileSystem.GetFiles(Form1.proyectPath, FileIO.SearchOption.SearchAllSubDirectories, "*.index")
         Dim fileReader As String = My.Computer.FileSystem.ReadAllText(fileIndex.First)
-        If fileReader <> "" Then
-            Dim fileNames() As String = fileReader.Split("|")
-            For chapters As Integer = 0 To fileNames.GetUpperBound(0)
-                lstBox_chapters.Items.Add(fileNames(chapters))
-            Next
-        End If
+        If fileReader <> "" Then fileReader = fileReader.TrimEnd("|")
+        Dim fileNames() As String = fileReader.Split("|")
+        For chapters As Integer = 0 To fileNames.GetUpperBound(0)
+            lstBox_chapters.Items.Add(fileNames(chapters))
+        Next
         lstBox_chapters.ClearSelected()
     End Sub
 
-    Private Sub btn_indexing_Click(sender As Object, e As EventArgs) Handles btn_indexing.Click
-        reindexing()
-    End Sub
     Private Sub reindexing()
         Dim index
         For Each chapter In lstBox_chapters.Items
             index = index & chapter & "|"
         Next
-        index = index.TrimEnd("|")
-        MsgBox(index)
-        My.Computer.FileSystem.WriteAllText(Form1.proyectPath & "\" & IO.Path.GetFileName(Form1.proyectPath) & ".index", index, False)
+        My.Computer.FileSystem.WriteAllText(Form1.proyectPath & "\" & Form1.proyectFolder("index"), index, False)
+        Form1.reloadChapters()
     End Sub
+
 End Class
